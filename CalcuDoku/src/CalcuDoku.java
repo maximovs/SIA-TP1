4451785308d063958456ff8b0651143809570304
kvnknny <knny.kvn@gmail.com>
2013-03-15 20:51:26 -0300

    Saque el main del parser (me habia oldidado)

---
+++ CalcuDoku/src/CalcuDoku.java
@@ -0,0 +1,11 @@
+import parser.Parser;
+import board.Board;
+
+
+public class CalcuDoku {
+	
+	public static void main(String args[]){
+		Board b = Parser.parse("levels/test.txt");
+	}
+
+}
--- CalcuDoku/src/parser/Parser.java
+++ CalcuDoku/src/parser/Parser.java
@@ -7,15 +7,23 @@ import group.MultiplyGroup;
 import group.SubstractGroup;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 import board.Board;
 
 public class Parser {
-	public static void main(String [ ] args) throws IOException{
+	
+	public static Board parse(String fileName){
 		
-		FileReader input = new FileReader("levels/test.txt");
+		FileReader input;
+		try {
+			input = new FileReader(fileName);
+		} catch (FileNotFoundException e) {
+			System.out.println("ARCHIVO INCORRECTO!");
+			return null;
+		}
 		BufferedReader bufReader = new BufferedReader(input);
 		String line = null;
 		
@@ -23,54 +31,67 @@ public class Parser {
 		Integer result = null;
 		String operation = null;
 		
-		line = bufReader.readLine();
+		try {
+			line = bufReader.readLine();
+		} catch (IOException e) {
+			System.out.println("ARCHIVO INCORRECTO!");
+			return null;
+		}
 		blockSize = Integer.valueOf(line);
 		if(blockSize == null){
 			System.out.println("ARCHIVO INCORRECTO!");
-			return;
+			return null;
 		}
 		
 		Board board = new Board(blockSize);
 		
-		while((line = bufReader.readLine()) != null){
-			String splited[] = line.split(",");
-			if(splited.length != 3){
-				System.out.println("ARCHIVO INCORRECTO!");
-				return;
-			}
-			result = Integer.valueOf(splited[0]);
-			operation = splited[1];
-			Group group;
-			switch(operation.charAt(0)){
-				case '/': group = new DivideGroup(result, blockSize);break;
-				case '*': group = new MultiplyGroup(result, blockSize);break;
-				case '+': group = new AddGroup(result, blockSize);break;
-				case '-': group = new SubstractGroup(result, blockSize);break; 
-				default:  System.out.println("ARCHIVO INCORRECTO!");return;
-			}
-			board.addGroup(group);
-			
-			String cells[] = splited[2].split(";");
-			for(String cell: cells){
-				String[] coord = cell.split(":");
-				if(coord.length != 2){
+		try {
+			while((line = bufReader.readLine()) != null){
+				String splited[] = line.split(",");
+				if(splited.length != 3){
 					System.out.println("ARCHIVO INCORRECTO!");
-					return;
+					return null;
 				}
-				Integer x = Integer.valueOf(coord[0]);
-				Integer y = Integer.valueOf(coord[1]);
-				if(x == null || y == null){
-					System.out.println("ARCHIVO INCORRECTO!");
-					return;
+				result = Integer.valueOf(splited[0]);
+				operation = splited[1];
+				Group group;
+				switch(operation.charAt(0)){
+					case '/': group = new DivideGroup(result, blockSize);break;
+					case '*': group = new MultiplyGroup(result, blockSize);break;
+					case '+': group = new AddGroup(result, blockSize);break;
+					case '-': group = new SubstractGroup(result, blockSize);break; 
+					default:  System.out.println("ARCHIVO INCORRECTO!");return null;
 				}
-				group.addCell(board.getCell(x, y));
+				board.addGroup(group);
+				
+				String cells[] = splited[2].split(";");
+				for(String cell: cells){
+					String[] coord = cell.split(":");
+					if(coord.length != 2){
+						System.out.println("ARCHIVO INCORRECTO!");
+						return null;
+					}
+					Integer x = Integer.valueOf(coord[0]);
+					Integer y = Integer.valueOf(coord[1]);
+					if(x == null || y == null){
+						System.out.println("ARCHIVO INCORRECTO!");
+						return null;
+					}
+					group.addCell(board.getCell(x, y));
+				}
+				
 			}
-			
+		} catch (Exception e) {
+			System.out.println("ARCHIVO INCORRECTO!");
+			return null;
 		}
+		
 		if(!board.isCorrect()){
 			System.out.println("ARCHIVO INCORRECTO!");
-			return;
+			return null;
 		}
+		System.out.println("Parser ok");
+		return board;
 	}
 
 }
