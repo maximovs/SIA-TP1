c646b880fc5cf0503c8da11c4e3cab3f3126fc3c
Maximo <maximovs@gmail.com>
2013-03-21 14:30:29 -0300

    2 leves added

--- CalcuDoku/src/calcudoku/CalcuDoku.java
+++ CalcuDoku/src/calcudoku/CalcuDoku.java
@@ -8,13 +8,15 @@ import gps.SearchStrategy;
 public class CalcuDoku {
 	
 	public static void main(String args[]){
+		// QuadOp3x3
+		// QuadOp4x4
+		// 5x5
 		// QuadOp6x6
 		// DualOp8x8
-		// 5X5.txt
-		CalcuDokuProblem problem = new CalcuDokuProblem("levels/DualOp8x8");
+		CalcuDokuProblem problem = new CalcuDokuProblem("levels/QuadOp3x3");
 		GPSEngine engine = new MultipleGPSEngine();
 		long a = System.currentTimeMillis();
-		engine.engine(problem, SearchStrategy.BFS);
+		engine.engine(problem, SearchStrategy.DFS);
 		System.out.println((System.currentTimeMillis()-a)/1000  + "segundos");
 	}
 
