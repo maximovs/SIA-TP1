180f69f02956e51f91bc5f5cfe26ce480e1cd32f
kvnknny <knny.kvn@gmail.com>
2013-03-23 18:12:12 -0300

    dfs iterativo

--- CalcuDoku/src/main/java/sia/gps/DFSEngine.java
+++ CalcuDoku/src/main/java/sia/gps/DFSEngine.java
@@ -2,10 +2,26 @@ package sia.gps;
 
 import java.util.Deque;
 
+import sia.calcudoku.CalcuDokuState;
+
 public class DFSEngine extends GPSEngine{
+
+	private int limit = 0;
+	
+	public DFSEngine() {
+		super();
+	}
+	
+	public DFSEngine(int limit) {
+		this.limit = limit;
+	}
 
 	@Override
 	public void addNode(GPSNode node) {
+		if( ((CalcuDokuState)node.getState()).getDepth() > limit ){
+			//Se alcanzo la altura maxima
+			return;
+		}
 		visited.add(node.getState());
 			@SuppressWarnings("unchecked")
 			Deque<GPSNode> dequeue = (Deque<GPSNode>) open;
