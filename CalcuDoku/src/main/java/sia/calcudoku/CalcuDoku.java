ff60f06678539a75adbd1774e60c75a6d1c9e5f3
Matias <arganaraz.matias@gmail.com>
2013-03-25 04:05:52 -0300

    Update CalcuDoku.java

--- CalcuDoku/src/main/java/sia/calcudoku/CalcuDoku.java
+++ CalcuDoku/src/main/java/sia/calcudoku/CalcuDoku.java
@@ -12,9 +12,10 @@ import sia.gps.IDFSEngine;
 public class CalcuDoku {
 	
 	public static void main(String args[]){
-		while (args == null || args.length < 3) {
+		if (args == null || args.length < 3) {
 			System.out.println("Formato de argumentos invalido");
 			System.out.println("Ingrese [BFS|DFS|IDFS|HIDFS|Greedy|Astar] [QuadOp3x3|QuadOp4x4|QuadOp4x4tricky|5x5|QuadOp6x6|DualOp8x8] [width] [groups|rowsNCols|both] [limitIDFS]");
+			return;
 		}
 		for(String s:args){
 			System.out.println(s);
